08b33ee41bf6a6d1df0fa6299b4e214632739afd
Jayasankar K M <jayasankar.km@vaisesika.local>
2025-05-26 16:58:46 +0530

    initial commit

--- Daily Tasks/task_1.py
+++ Daily Tasks/task_1.py
@@ -1,29 +1,7 @@
-# lst=[1,2,2,3,1,4,1,2,4,3,1,5,2]
-
-# dict={}
-
-# for i in lst:
-
-#     if i not in dict:
-
-#         dict[i]=1
-
-#     else:
-
-#         dict[i]+=1
-
-
-# max_item=max(dict.values())
-
-# max_keys=[k for k,v in dict.items() if v==max_item]
-
-# print(max_item)
-
-#optimized code
-
-lst=[1,2,2,3,1,4,1,2,4,3,1,5,2]
 
 from collections import Counter
+
+lst=[1,2,2,3,1,4,1,2,4,3,1,5,2]
 
 count=Counter(lst)
 
